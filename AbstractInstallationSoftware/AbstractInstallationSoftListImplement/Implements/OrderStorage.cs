ff88cd5c9591bc70d8ac03732dbdbb31660b3ad0
aleca <aleca2002@yandex.ru>
2021-03-07 16:29:55 +0300

    fix conflicts

--- AbstractInstallationSoftware/AbstractInstallationSoftListImplement/Implements/OrderStorage.cs
+++ AbstractInstallationSoftware/AbstractInstallationSoftListImplement/Implements/OrderStorage.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AbstractIntstallationSoftwareListImplement.Implements
+namespace AbstractInstallationSoftListImplement.Implements
 {
     public class OrderStorage : IOrderStorage
 

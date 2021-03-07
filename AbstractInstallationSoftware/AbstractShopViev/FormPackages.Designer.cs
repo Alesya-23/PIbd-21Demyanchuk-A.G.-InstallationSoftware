b55c0c4ba1df703dca0adbcb430c004d8cfda756
aleca <aleca2002@yandex.ru>
2021-03-07 16:32:53 +0300

    fix conflicts

--- AbstractInstallationSoftware/AbstractShopViev/FormPackages.Designer.cs
+++ AbstractInstallationSoftware/AbstractShopViev/FormPackages.Designer.cs
@@ -97,7 +97,7 @@
             this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.buttonAdd);
             this.Name = "FormPackages";
-            this.Text = "Форма пакетов";
+            this.Text = "Изделия";
             this.Load += new System.EventHandler(this.FormPackages_Load);
             this.Click += new System.EventHandler(this.FormPackages_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
